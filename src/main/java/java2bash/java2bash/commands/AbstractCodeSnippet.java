f4049b29d128b99d171c00f7fb54f487338a9589
Basil Abbas <basilabbas@hotmail.com>
2016-03-23 11:37:05 +0200

    Saving.

--- src/main/java/java2bash/java2bash/commands/AbstractCodeSnippet.java
+++ src/main/java/java2bash/java2bash/commands/AbstractCodeSnippet.java
@@ -6,14 +6,14 @@ import java.util.List;
 public abstract class AbstractCodeSnippet implements Snippet {
 	
 	private List<String> EMPTY_LIST = new ArrayList<>(0);
-
+	
 	@Override
 	public String getCleanupCode() {
 		return null;
 	}
 
 	@Override
-	public List<String> getUniqueCode() {
+	public List<String> getIncludesList() {
 		return EMPTY_LIST;
 	}
 }
