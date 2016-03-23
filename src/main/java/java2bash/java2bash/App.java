990db75c49978dc5621fd8584fd451cd7fa34538
Basil Abbas <basilabbas@hotmail.com>
2016-03-23 11:37:39 +0200

    Sample example renames.

--- src/main/java/java2bash/java2bash/App.java
+++ src/main/java/java2bash/java2bash/App.java
@@ -2,8 +2,11 @@ package java2bash.java2bash;
 
 import java.util.EnumSet;
 
-import java2bash.java2bash.commands.simple.Echo;
-import java2bash.java2bash.commands.simple.EchoColor;
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Strings;
+
+import java2bash.java2bash.commands.simple.Echo2;
+import java2bash.java2bash.commands.simple.Color;
 import java2bash.java2bash.commands.simple.PauseSnippet;
 import java2bash.java2bash.commands.simple.SimpleCodeSnippet;
 
@@ -15,13 +18,14 @@ public class App
 {
     public static void main( String[] args )
     {
+    	
     	BashScript bashScript = new BashScript();
     	bashScript.add(new SimpleCodeSnippet("echo \"HELLO WORLD!\""));
     	
-    	bashScript.add(new Echo(
-    			"Deployment Automation Checklist", EnumSet.of(EchoColor.WHITE, EchoColor.RED_BG, EchoColor.BOLD)));
-    	bashScript.add(new Echo(
-   			"Will now pull the latest git using 'git checkout' and pull them well, then I will try my best to create a repository synchronized. Thats all there is to have this working correctly or not. Hope this works.", EchoColor.CYAN));
+    	bashScript.add(new Echo2(
+    			"Deployment Automation Checklist", EnumSet.of(Color.WHITE, Color.RED_BG, Color.BOLD)));
+    	bashScript.add(new Echo2(
+   			"Will now pull the latest git using 'git checkout' and pull them well, then I will try my best to create a repository synchronized. Thats all there is to have this working correctly or not. Hope this works.", Color.CYAN));
     	bashScript.add(new PauseSnippet());
     	
 		System.out.println(bashScript.render());
